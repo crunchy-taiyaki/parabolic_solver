6470e9fe7890648dc0b649c1032c66f9f7f69d0b
Marta <marta.obolentseva@yandex.ru>
2020-12-13 15:42:34 +0300

    comments deleted

--- parabolic_solver/main.f90
+++ parabolic_solver/main.f90
@@ -16,33 +16,11 @@ program parabolic_equation
     allocate(solution(0:N,0:M))
     allocate(residuals(0:N,0:M))
     
-    !test beta
-    !call coord_grid(x,t,N,M)
-    !do k=0,M
-    !    write(*,*) beta(t(k)), t(k)**3 + 4.0_mp
-    !enddo
-    
-    !test L
-    !call coord_grid(x,t,N,M)
-    !do k=0,M
-    !do i=0,N
-    !    write(*,*)k,i, L(x(i),t(k)) - (9.0_mp*x(i)**2 + 6.0_mp*x(i) - u_ref(x(i),t(k)))
-    !enddo
-    !enddo
-    
-    !test f(x,t)
-    !call coord_grid(x,t,N,M)
-    !do k=0,M
-    !do i=0,N
-    !    write(*,*)k,i, f(x(i),t(k)) - (3.0_mp*t(k)**2 - (9.0_mp*x(i)**2 + 6.0_mp*x(i) - u_ref(x(i),t(k))))
-    !enddo
-    !enddo
-    
     call parabolic_solver(N,M,phi,f,Lh_x_u,alpha,alpha1,alpha2,beta,beta1,beta2,sigma,x,t,solution)
     do k=0,M
         do i=0,N
             residuals(i,k) = solution(i,k) - u_ref(x(i),t(k))
-            write(*,*)k,i,residuals(i,k) !x(i),solution(i,k),
+            write(*,*)k,i,residuals(i,k)
         enddo
     enddo
     end program
